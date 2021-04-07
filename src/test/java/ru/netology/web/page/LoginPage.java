49b8bd7114832b78273da7dce1a09c1027d54085
EliseevG787 <rdn787@bk.ru>
2021-04-07 17:35:32 +0300

    added LoginPage.java

---
+++ src/test/java/ru/netology/web/page/LoginPage.java
@@ -0,0 +1,40 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.web.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class LoginPage {
+    private SelenideElement loginField = $("[data-test-id=login] input");
+    private SelenideElement passwordField = $("[data-test-id=password] input");
+    private SelenideElement loginButton = $("[data-test-id=action-login]");
+    private SelenideElement errorNotification = $("[data-test-id=error-notification]");
+    private SelenideElement emptyLogin = $("[data-test-id=login] .input__sub");
+    private SelenideElement emptyPassword = $("[data-test-id=password] .input__sub");
+
+    public LoginPage() {
+        loginField.shouldBe(visible);
+    }
+
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        loginField.setValue(info.getLogin());
+        passwordField.setValue(info.getPassword());
+        loginButton.click();
+        return new VerificationPage();
+    }
+
+    public void invalidLogin(DataHelper.AuthInfo info) {
+        loginField.setValue(info.getLogin());
+        passwordField.setValue(info.getPassword());
+        loginButton.click();
+        errorNotification.shouldBe(visible);
+    }
+
+    public void emptyLogin() {
+        loginButton.click();
+        emptyLogin.shouldBe(visible);
+        emptyPassword.shouldBe(visible);
+    }
+}
