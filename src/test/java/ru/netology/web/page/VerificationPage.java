8a883157a553e4b6091c790e6d8cca9440e3bd83
EliseevG787 <rdn787@bk.ru>
2021-04-07 17:36:57 +0300

    added VerificationPage.java

---
+++ src/test/java/ru/netology/web/page/VerificationPage.java
@@ -0,0 +1,35 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.web.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class VerificationPage {
+    private SelenideElement codeField = $("[data-test-id=code] input");
+    private SelenideElement verifyButton = $("[data-test-id=action-verify]");
+    private SelenideElement errorNotification = $("[data-test-id=error-notification]");
+    private SelenideElement emptyCode = $("[data-test-id=code].input_invalid .input__sub");
+
+    public VerificationPage() {
+        codeField.shouldBe(visible);
+    }
+
+    public DashboardPage validVerify(DataHelper.VerificationCode verificationCode) {
+        codeField.setValue(verificationCode.getCode());
+        verifyButton.click();
+        return new DashboardPage();
+    }
+
+    public void invalidVerify(DataHelper.VerificationCode verificationCode) {
+        codeField.setValue(verificationCode.getCode());
+        verifyButton.click();
+        errorNotification.shouldBe(visible);
+    }
+
+    public void emptyVerify() {
+        verifyButton.click();
+        emptyCode.shouldBe(visible);
+    }
+}
